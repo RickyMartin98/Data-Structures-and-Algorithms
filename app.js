17004fc36e65007b4a961fb6d34321a8db9d473d
Martin Fernandez <mfernandez2@ucol.mx>
2021-10-30 03:14:02 -0500

    development of pop, push and size functions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const myObject = {
     age: 22
 };
 
-for(let prop in myObject ) {
+for (let prop in myObject) {
     console.log(` ${prop}: ${myObject[prop]}`);
 }
 
@@ -28,19 +28,16 @@ var myParentsHouse = new Building(2);
 myParentsHouse.countFloors();
 
 
-function Person (name, age) {
+function Person(name, age) {
     this.name = name;
     this.age = age;
 }
 
 Person.prototype.greeting = function () {
-    if (this.age >= 18) {
+    if (this.age >= 18)
         console.log(`Hello my names is ${this.name} and I'm and adult`);
-    } 
-    else  
-    {
+    else
         console.log(`Hello my name is ${this.name} and I'm a kid`);
-    }
 }
 
 let Juan = new Person("Juan", 20);
@@ -50,3 +47,60 @@ Juan.greeting();
 Miguel.greeting();
 
 //blueprint -> plano
+
+function Stack() {
+    this.storage = "";
+}
+/**
+ * Adding value to storage property
+ * @params { string } the value to be added in the property
+ * 
+ */
+
+Stack.prototype.push = function (val) {
+    //first validate if property it's empty
+    if (this.storage == "")
+        this.storage = val;
+    else
+        this.storage += "," + val;
+}
+
+/**
+ * for pop function need to ask if property is a string to split and slice it (convert it to an array).
+ * if it's not we only slice the last item of the array
+*/
+
+Stack.prototype.pop = function () {
+    if (typeof this.storage == 'string')
+        this.storage = this.storage.split(',').slice(0, -1);
+    else
+        this.storage = this.storage.slice(0, -1);
+}
+/**
+ * we return the length value from the array already converted
+ * @returns {number} the length of array.
+ * 
+ */
+Stack.prototype.size = function () {
+    //returning the length value from storage split by ","
+    if (this.storage == 'string') 
+        return this.storage.split(",").length;   
+        
+    return this.storage.length;
+}
+
+var myWeeklyMenu = new Stack();
+
+myWeeklyMenu.push("ReadBeans");
+
+myWeeklyMenu.push("Banana");
+myWeeklyMenu.push("Orange");
+myWeeklyMenu.push("Peach");
+myWeeklyMenu.push("Lemon");
+console.log(myWeeklyMenu);
+
+myWeeklyMenu.pop();
+myWeeklyMenu.pop();
+
+console.log(myWeeklyMenu.size());
+console.log(myWeeklyMenu);
